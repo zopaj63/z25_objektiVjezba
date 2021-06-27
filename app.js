1ac135a2b7c669ccece3f24f9881a95bcf2514f7
zopaj63 <zoran.pajnic@gmail.com>
2021-06-27 15:28:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*
 VJEŽBA: Objekti
-TRAJANJE: 45min
+TRAJANJE: cca 45min (cca do 19:35)
 1. Kreiraj objekt koji reprezentira neki specifični auto, sa bar 8 svojstava, od kojih su 3 funkcije (npr. ubrzaj, zakoci itd.), a jedna je također objekt. Probajte referencirati druge vrijednosti objekta u funkcijama. Svaka funkcija mora primati argumente i vracati neku vrijednost.
 2. Pozovite funkcije svog objekta sa argumentima i ispisite rezultat.
 3. Sa petljom pristupi svim brojivim svojstvima tvog auto objekta i ispiši vrijednosti u konzoli zajedno sa njihovim imenima (kljucevima/property) (npr. ime: Ivan)
@@ -8,7 +8,6 @@ TRAJANJE: 45min
 4. Ispiši u konzoli razumljivu rečenicu koja uključuje bar 2 svojstva tvog objekta.
 5. Pretvori svoj objekt u JSON string.
 (GOOGLE: MDN JSON stringify)
-
 BONUS:
 Tko hoće može podići na GitHub :)
 */
